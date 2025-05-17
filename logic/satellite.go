1c588c1a979ad611f268445b809e59bb4e50e44e
hong datou <2744423541@qq.com>
2025-05-17 04:15:51 +0000

    fix:修复切换卫星模态的api

--- logic/satellite.go
+++ logic/satellite.go
@@ -16,8 +16,8 @@ func (m *Manager) SwitchSatelliteHandler(ctx *context.Context) {
 		log.Errorf("SwitchSatelliteHandler参数绑定错误: %v", err)
 		return
 	}else{
-		//解析到参数，执行武大提供的脚本 ssh luci@192.168.2.201 'sudo bash /home/luci/Desktop/ovsconf.sh  XXX'
-		cmd:= exec.Command("ssh", "luci@192.168.2.201", "sudo bash /home/luci/Desktop/ovsconf.sh", fmt.Sprintf("%s", req.SwitchID))
+		//解析到参数，执行武大提供的脚本ssh -o PreferredAuthentications=publickey -o PasswordAuthentication=no luci@192.168.2.201 'sudo bash /home/luci/Desktop/ovsconf.sh  XXX'
+		cmd:= exec.Command("ssh", "-o", "PreferredAuthentications=publickey", "-o", "PasswordAuthentication=no", "luci@192.168.2.201", "sudo bash /home/luci/Desktop/ovsconf.sh", fmt.Sprintf("%s", req.SwitchID))
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			log.Errorf("SwitchSatelliteHandler脚本执行错误: %v", err)

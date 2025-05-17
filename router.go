199545696a67e267c7c0ddf8f58b17f676ea8020
hong datou <2744423541@qq.com>
2025-05-17 03:31:45 +0000

    feat:添加切换卫星模态的API。

--- router.go
+++ router.go
@@ -26,5 +26,6 @@ func setupRouter(manager *logic.Manager) {
 
 	web.Get("/api/flows", manager.PrepareFlowsHandler) // 根据三元信息计算需要下发的流表项
 
+	web.Post("/api/switchsatellite", manager.SwitchSatelliteHandler) // 切换卫星模态
 	web.Router("/", &MainController{})
 }

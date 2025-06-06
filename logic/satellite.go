8b756730291b3bb220aaeed7e4d707a7860fe6ea
hong datou <2744423541@qq.com>
2025-06-06 02:34:06 +0000

    fix:修复执行切换卫星脚本的语句。

--- logic/satellite.go
+++ logic/satellite.go
@@ -1,22 +1,25 @@
 package logic
 
 import (
+	"os/exec"
+
 	"github.com/beego/beego/v2/server/web/context"
 	log "github.com/sirupsen/logrus"
-	"os/exec"
 )
+
 func (m *Manager) SwitchSatelliteHandler(ctx *context.Context) {
 	type SwitchSatelliteRequest struct {
-		SwitchID  string  `json:"switch_id"`
+		SwitchID string `json:"switch_id"`
 	}
 	var req SwitchSatelliteRequest
 	if err := ctx.BindJSON(&req); err != nil {
 		responseError(ctx, err)
 		log.Errorf("SwitchSatelliteHandler参数绑定错误: %v", err)
 		return
-	}else{
+	} else {
 		//解析到参数，执行武大提供的脚本ssh luci@192.168.2.201 'sudo bash /home/luci/Desktop/ovsconf.sh  XXX'
-		cmd := exec.Command("sshpass", "-p", "Fiberhome@2020", "ssh", "kin@10.190.96.114", "sudo", "-S", "bash", "/home/kin/Desktop/ovsconf.sh", req.SwitchID)
+		cmd := exec.Command("sshpass", "-p", "Fiberhome@2020", "ssh", "-t", "onos4@10.190.96.114", "sudo", "/home/onos4/weixing/ovsconf.sh", req.SwitchID)
+		// cmd := exec.Command("ssh", "onos4@10.190.96.114", "sudo", "bash", "/home/onos4/weixing/ovsconf.sh", req.SwitchID)
 		//设置命令的输出
 		output, err := cmd.CombinedOutput()
 		if err != nil {
